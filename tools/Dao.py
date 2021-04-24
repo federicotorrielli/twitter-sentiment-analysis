212630515a2985079328dd36926edba64b3ea5b2
ivsnp <65447880+ivsnp@users.noreply.github.com>
2021-04-24 17:14:19 +0200

    dao

--- tools/Dao.py
+++ tools/Dao.py
@@ -4,7 +4,7 @@ import toml
 
 class Dao:
     def __init__(self):
-        auth = toml.load("auth/auth.toml")
+        auth = toml.load("../auth/auth.toml")
         self.user = auth.get("user")
         self.pwd = auth.get("password")
         self.host = auth.get("host")
@@ -15,7 +15,8 @@ class Dao:
                              user=self.user,
                              passwd=self.pwd,
                              db=self.db,
-                             charset='utf32',
+                             port=3306,
+                             charset='utf8',
                              cursorclass=pymysql.cursors.SSCursor)
         cur = db.cursor()
         cur.execute('SET NAMES utf8mb4')
@@ -29,8 +30,13 @@ class Dao:
         cursor = self.__query_db()
         res = []
         try:
-            cursor.execute('SELECT * FROM table')
+            cursor.execute('SELECT * FROM sentiment')
             res = [t[0] for t in cursor.fetchall()]
         finally:
             cursor.close()
         return res
+
+
+if __name__ == '__main__':
+    """ DAO tests. """
+    print(Dao().query_example())
