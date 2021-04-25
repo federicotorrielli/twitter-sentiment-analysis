e209b7f9ad9722e6d121d217751af078049c8be4
Federico Torrielli <evilscript@protonmail.com>
2021-04-25 11:02:58 +0200

    Natural.py continuation and emoticons init

--- src/Natural.py
+++ src/Natural.py
@@ -1,18 +1,26 @@
-from nltk import tokenize, download
+from nltk import download
 from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
 from glob import glob
 from FileManager import *
+from Resources.lexical.set_classification import *
+
+# TODO: check reduce_len and preserve_case
+tokenizer = TweetTokenizer(reduce_len=True, preserve_case=False)
 
 
 def process_phrase(phrase: str, stopword_list: set):
-    tokenized = tokenize.word_tokenize(phrase)
+    tokenized = tokenizer.tokenize(phrase)
     final_phrase = [word for word in tokenized if word not in stopword_list]
+    # TODO: tokenize emoticons correctly and continue the program
     print(final_phrase)
 
 
 def create_stopword_list() -> set:
     default = stopwords.words('english')
-    return set(default)
+    personal = twitter_stopwords
+    final = default + personal
+    return set(final)
 
 
 def process_dataset(file_path: str):
