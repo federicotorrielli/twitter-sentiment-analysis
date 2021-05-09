04cba89acfa2430fe3762fe382887fc518b5f76d
ivsnp <65447880+ivsnp@users.noreply.github.com>
2021-05-09 19:27:13 +0200

    Build DB: drop/create, sentiments, words, emoticons, emojis

--- src/Dao.py
+++ src/Dao.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import pymysql
 from toml import load
-from set_classification import posemoticons, negemoticons, EmojiPos, EmojiNeg, OthersEmoji
+from src.Natural import process_phrase, create_stopword_list
+from src.lexical_glob import get_sentiment_words, get_sentiment_emojis, get_sentiment_emoticons
 
 
 class Dao:
@@ -13,6 +14,10 @@ class Dao:
         self.db = auth.get("db")
 
     def __connect_db(self):
+        """
+        Connection to the Relational DB.
+        @return: the connection to the Relational DB
+        """
         connection = pymysql.connect(host=self.host,
                                      port=3306,
                                      user=self.user,
@@ -26,11 +31,19 @@ class Dao:
         """
         Builds all tables of the relational db
         """
-        # self.__drop_and_create_tables()
-        # self.__insert_tweets()
-        # self.__insert_words()
-        # self.__insert_sentiments()
-        # self.__insert_icons()
+        print("Building DB")
+        self.__drop_and_create_tables()
+        print("Adding sentiments")
+        self.__insert_sentiments()
+        print("Adding words")
+        self.__insert_words(get_sentiment_words())
+        print("Adding emoticons")
+        self.__insert_emoticons(get_sentiment_emojis())
+        print("Adding emojis")
+        self.__insert_emojis(get_sentiment_emoticons())
+
+        # insert tweets
+        # self.__insert_tweets(get_sentiment_tweets())
 
     def __drop_and_create_tables(self):
         """
@@ -81,8 +94,7 @@ class Dao:
 
                 cursor.execute("CREATE TABLE `labelled` ("
                                "`sentiment_id` int NOT NULL, "
-                               "`tweet_id` int NOT NULL, "
-                               "`count` int NOT NULL)")
+                               "`tweet_id` int NOT NULL)")
                 cursor.execute("ALTER TABLE `labelled`"
                                "ADD PRIMARY KEY (`sentiment_id`,`tweet_id`), "
                                "ADD KEY `tweet_id` (`tweet_id`);")
@@ -108,8 +120,8 @@ class Dao:
 
                 cursor.execute("CREATE TABLE `in_tweet` ("
                                "`tweet_id` int NOT NULL, "
-                               "`token_id` int NOT NULL, "
-                               "`count` int NOT NULL)")
+                               "`token_id` int NOT NULL,"
+                               "`type` SET('WORD','EMOJI','EMOTICON') NOT NULL)")
                 cursor.execute("ALTER TABLE `in_tweet` "
                                "ADD PRIMARY KEY (`tweet_id`,`token_id`);")
                 cursor.execute("ALTER TABLE `in_tweet` "
@@ -117,45 +129,6 @@ class Dao:
                                "FOREIGN KEY (`tweet_id`) REFERENCES `twitter_message` (`id`);")
                 connection.commit()
 
-    # TODO
-    def __insert_tweets(self):
-        """
-        Builds twitter_message table
-        """
-        with self.__connect_db() as connection:
-            with connection.cursor() as cursor:
-                cursor.execute('SET NAMES utf8mb4')
-                cursor.execute("SET CHARACTER SET utf8mb4")
-                cursor.execute("SET character_set_connection=utf8mb4")
-
-                # TODO get tweets tokenized from evilscript func
-                tweets = []
-                sql = "INSERT INTO `twitter_message` (`tweet_content`) VALUES (%s)"
-                for t in tweets:
-                    cursor.execute(sql, t)
-                    # TODO add sentiments (?)
-                connection.commit()
-
-    # TODO
-    def __insert_words(self):
-        """
-        Builds word table
-        """
-        with self.__connect_db() as connection:
-            with connection.cursor() as cursor:
-                cursor.execute('SET NAMES utf8mb4')
-                cursor.execute("SET CHARACTER SET utf8mb4")
-                cursor.execute("SET character_set_connection=utf8mb4")
-
-                # TODO get words classified from evilscript func
-                tweets = []
-                sql = "INSERT INTO `word` (`word`) VALUES (%s)"
-                for t in tweets:
-                    cursor.execute(sql, t)
-                    # TODO add sentiments (?)
-                connection.commit()
-
-    # TODO
     def __insert_sentiments(self):
         """
         Builds sentiment table
@@ -172,10 +145,51 @@ class Dao:
                     cursor.execute(sql, s)
                 connection.commit()
 
-    # TODO
-    def __insert_icons(self):
+    def __insert_words(self, word_sentiments: {}):
         """
-        Builds emoji and emoticon tables
+        Builds word table
+        """
+        with self.__connect_db() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SET NAMES utf8mb4')
+                cursor.execute("SET CHARACTER SET utf8mb4")
+                cursor.execute("SET character_set_connection=utf8mb4")
+
+                sql_word = "INSERT INTO `word` (`word`) VALUES "
+                sql_word_add = "(%s)"
+                sql_word_params = []
+                sql_belongs_to = "INSERT INTO `belongs_to`(`sentiment_id`, `word_id`) VALUES "
+                sql_belongs_to_add = "(%s, %s)"
+                sql_belongs_to_params = []
+
+                first_insertion = True
+                id_word = 0
+                for word in word_sentiments:
+                    id_word += 1
+                    if not first_insertion:
+                        sql_word += ", "
+                    sql_word += sql_word_add
+                    sql_word_params += [word]
+
+                    for index, sentiment in enumerate(word_sentiments[word]):
+                        if not first_insertion:
+                            sql_belongs_to += ", "
+
+                        sql_belongs_to += sql_belongs_to_add
+                        sql_belongs_to_params += [word_sentiments[word][index] + 1, id_word]
+                        first_insertion = False
+
+                cursor.execute(sql_word, sql_word_params)
+                connection.commit()
+                cursor.execute(sql_belongs_to, sql_belongs_to_params)
+                connection.commit()
+
+                print(f"{len(word_sentiments)} words added")
+                print(f"{sum([len(word_sentiments[k]) for k in word_sentiments])} relations added")
+
+    def __insert_emoticons(self, polarity_emoticons: {}):
+        """
+        Builds emoticon table
         """
         with self.__connect_db() as connection:
             with connection.cursor() as cursor:
@@ -184,28 +198,36 @@ class Dao:
                 cursor.execute("SET character_set_connection=utf8mb4")
 
                 sql = "INSERT INTO `emoticon` (`emoticon`, polarity) VALUES (%s, %s)"
-                for emoticon in posemoticons:
-                    cursor.execute(sql, (emoticon, 'positive'))
-                for emoticon in negemoticons:
-                    cursor.execute(sql, (emoticon, 'negative'))
+                for index in polarity_emoticons:
+                    for emoticon in polarity_emoticons[index]:
+                        cursor.execute(sql, (emoticon, index))
                 connection.commit()
 
+    def __insert_emojis(self, polarity_emojis: {}):
+        """
+        Builds emoji table
+        """
+        with self.__connect_db() as connection:
             with connection.cursor() as cursor:
                 cursor.execute('SET NAMES utf8mb4')
                 cursor.execute("SET CHARACTER SET utf8mb4")
                 cursor.execute("SET character_set_connection=utf8mb4")
 
                 sql = "INSERT INTO `emoji` (`emoji`, polarity) VALUES (%s, %s)"
-                for emoji in EmojiPos:
-                    cursor.execute(sql, (emoji, 'positive'))
-                for emoji in EmojiNeg:
-                    cursor.execute(sql, (emoji, 'negative'))
-                for emoji in OthersEmoji:
-                    cursor.execute(sql, (emoji, 'other'))
+                for index in polarity_emojis:
+                    for emoji in polarity_emojis[index]:
+                        cursor.execute(sql, (emoji, index))
                 connection.commit()
 
-    # TODO add single_query
-    # TODO add single_exec
+    # TODO
+    def __insert_tweets(self, tweets_content: [str]):
+        """
+        Builds twitter_message table
+        @param tweets_content: list of tweets' content
+        """
+
+    # TODO add __single_query(statement:str)
+    # TODO add __single_exec(statement:str)
 
 
 if __name__ == '__main__':
