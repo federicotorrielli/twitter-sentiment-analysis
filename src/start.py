b5b5db7e036339eed13add0cd8a363d87b9f2945
ivsnp <65447880+ivsnp@users.noreply.github.com>
2021-06-24 09:53:43 +0200

    tests

--- src/start.py
+++ src/start.py
@@ -11,7 +11,7 @@ def start_comparison(db_type="MongoDB"):
     print(f"Building {db_type}...")
     start = timer()
     spec_dao = Dao(db_type != "MongoDB")
-    spec_dao.build_db()
+    # spec_dao.build_db() TODO: uncomment
     end = timer()
     print(f"Done building {db_type} in {end - start} seconds")
 
@@ -38,10 +38,10 @@ def test_query(db_type="MongoDB"):
 
 
 if __name__ == '__main__':
-    dao = start_comparison("MongoDB")
-    # dao2 = start_comparison("MySQL")
+    # dao = start_comparison("MongoDB")
+    dao2 = start_comparison("MySQL")
     test_query()
 
-    if input("Do you want to generate the Wordclouds? This could take 10 minutes or more! [y/N] ").lower() == "y":
-        wordcl = WordCloudCreator(dao)
-        wordcl.generate()
+    # if input("Do you want to generate the Wordclouds? This could take 10 minutes or more! [y/N] ").lower() == "y":
+    #     wordcl = WordCloudCreator(dao)
+    #     wordcl.generate()
